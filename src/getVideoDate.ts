e2f3ebe580fc2f836cd038cb40fe3172a4ab57e4
valere <dev@valerebron.com>
2023-04-03 13:44:08 +0200

    fix: revert token & add new date managing

--- src/getVideoDate.ts
+++ src/getVideoDate.ts
@@ -5,7 +5,10 @@ export default async function getVideoDate(id: string) {
   try {
     const youtube = new Client()
     const data = await youtube.getVideo(id)
-    return dayjs(data.uploadDate).toDate()
+    let stringDate = data.uploadDate.replace(/^\D+/,'')
+    stringDate = stringDate.replace('janv.', 'jan').replace('févr.', 'feb').replace('mars', 'mar').replace('avr.', 'apr').replace('mai', 'may').replace('juin', 'jun').replace('juil.','jul').replace('août','aug').replace('déc.','dec')
+    const finalDate = dayjs(stringDate).toDate()
+    return finalDate
   } catch(e) {
     console.log('cannot get date for '+id+', try again')
     // console.log(e)
